2554adfe1da1abaac053f741fc0ef13a7f8de086
dudekulasaddu125022-hash <dudekulasaddu.125022@marwadiuniversity.ac.in>
2026-09-09 21:40:58 +0530

    update

---
+++ practical 6/c code/matrix chain mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    printf("Enter number of matrices: ");
+    scanf("%d", &n);
+
+    int p[n + 1], m[n + 1][n + 1];
+
+    printf("Enter %d dimensions: ", n + 1);
+    for (int i = 0; i <= n; i++)
+        scanf("%d", &p[i]);
+
+    for (int i = 1; i <= n; i++)
+        m[i][i] = 0;
+
+    for (int len = 2; len <= n; len++) {
+        for (int i = 1; i <= n - len + 1; i++) {
+            int j = i + len - 1;
+            m[i][j] = INT_MAX;
+
+            for (int k = i; k < j; k++) {
+                int cost = m[i][k] + m[k + 1][j]
+                         + p[i - 1] * p[k] * p[j];
+
+                if (cost < m[i][j])
+                    m[i][j] = cost;
+            }
+        }
+    }
+
+    printf("Minimum multiplications = %d\n", m[1][n]);
+
+    return 0;
+}
+
+
